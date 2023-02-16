16a5d57ebb8e773c706d3379f1b00451e5603b93
abhishek <purohitabhishek308@gmail.com>
2023-02-16 23:57:02 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import { BrowserRouter as Router  , Routes, Route , Link} from 'react-router-dom
 import {Home} from './pages/home';
 import { ContantUs } from './pages/contactus';
 import { Menu } from './pages/menu';
+import Navbar from "./pages/components/Navbar.js"
 function App() {
   
   return (
     <div className="App">
       <Router>
         <div>
-          Navbar
+          <Navbar/>
           <Link to={"/"}>home</Link>
           <Link to={"/menu"}>Menu</Link>
           <Link to={"/contactus"}>contactus</Link>
         </div>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/menu" element={<Menu/>}/>
-          <Route path="/contactus" element={<ContantUs/>}/>
+          
         </Routes>
       </Router>
       
